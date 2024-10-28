8bc41b48f717ae30b7bc60640f92ee76ac14a30d
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-10-28 09:45:01 +0800

    Upto Lesson 14

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,15 +1,33 @@
 //Shortcut
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll() {
-        return 'This action returns all coffees';
+	constructor(private readonly coffeesService: CoffeesService) {}
+
+	@Get()
+	findAll() {
+		return this.coffeesService.findAll();
+	}
+
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.coffeesService.findOne(id);
+	}
+
+	@Post()
+	create(@Body() body) {
+		return this.coffeesService.create(body);
+	}
+
+	@Patch(':id')
+	update(@Param('id') id: string, @Body() body) {
+		return this.coffeesService.update(id, body);
+	}
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.coffeesService.remove(id);
     }
-    
-    @Get(':id')
-    findOne(@Param('id') id:string) {
-        return `This action returns #${id} coffees`;
-    }
-} 
+}
